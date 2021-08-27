2013aafa8291916c68a11bd8b347672885bad5dc
André Carvalho <andre.daniel.carvalho@gmail.com>
2021-08-27 13:28:59 +0200

    If size is negative throw an exception

--- android/app/src/main/java/pt/carvalho/apples/classifier/utilities/BitmapUtilities.kt
+++ android/app/src/main/java/pt/carvalho/apples/classifier/utilities/BitmapUtilities.kt
@@ -5,12 +5,15 @@ import java.lang.IllegalArgumentException
 import kotlin.math.min
 
 @Throws(IllegalArgumentException::class)
-fun Bitmap.centerSquareCrop(size: Int) : Bitmap {
+fun Bitmap.centerSquareCrop(size: Int): Bitmap {
     val smallestSide = min(width, height)
 
-    if (smallestSide < size) {
+    if (smallestSide < size || size < 0) {
         throw IllegalArgumentException("Expected size of bitmap is bigger than the actual bitmap.")
     }
 
-    return Bitmap.createBitmap(this, width / 2 - height / 2, 0, size, size)
+    val x = (width - size) / 2
+    val y = (height - size) / 2
+
+    return Bitmap.createBitmap(this, x, y, size, size)
 }
