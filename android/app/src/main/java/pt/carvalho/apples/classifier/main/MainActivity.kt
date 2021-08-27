355f75ea8e5d32156fe76a4857346af6f10457cc
André Carvalho <andre.daniel.carvalho@gmail.com>
2021-08-27 17:33:53 +0200

    Adds hilt

--- android/app/src/main/java/pt/carvalho/apples/classifier/main/MainActivity.kt
+++ android/app/src/main/java/pt/carvalho/apples/classifier/main/MainActivity.kt
@@ -1,16 +1,21 @@
-package pt.carvalho.apples.classifier
+package pt.carvalho.apples.classifier.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.Modifier
+import dagger.hilt.android.AndroidEntryPoint
 import pt.carvalho.apples.classifier.processing.FrameConvertor
 import pt.carvalho.apples.classifier.ui.CameraPreview
 import pt.carvalho.apples.classifier.utilities.fullscreen
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         fullscreen()
@@ -19,8 +24,7 @@ class MainActivity : ComponentActivity() {
             MaterialTheme {
                 CameraPreview(
                     modifier = Modifier.fillMaxSize(),
-                    analyzer = FrameConvertor {
-                    }
+                    analyzer = FrameConvertor(viewModel::process)
                 )
             }
         }
