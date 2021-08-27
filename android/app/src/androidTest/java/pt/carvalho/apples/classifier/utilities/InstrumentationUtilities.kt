01545bf91f9169853e420f5ea24299db18b38341
André Carvalho <andre.daniel.carvalho@gmail.com>
2021-08-27 14:02:54 +0200

    Remove unused function

--- android/app/src/androidTest/java/pt/carvalho/apples/classifier/utilities/InstrumentationUtilities.kt
+++ android/app/src/androidTest/java/pt/carvalho/apples/classifier/utilities/InstrumentationUtilities.kt
@@ -1,15 +1,9 @@
 package pt.carvalho.apples.classifier.utilities
 
 import androidx.test.platform.app.InstrumentationRegistry
-import java.io.File
 import java.io.InputStream
-
-internal val filesDir: File
-    get() = InstrumentationRegistry.getInstrumentation()
-        .targetContext.filesDir
 
 internal fun assets(name: String): InputStream {
     return InstrumentationRegistry.getInstrumentation()
         .context.resources.assets.open(name)
 }
-
