378ef6563cf260c6a140a30e336993d5fe05e6a8
André Carvalho <andre.daniel.carvalho@gmail.com>
2021-08-27 13:48:53 +0200

    Resize image when bitmap size is smaller than the wanted size

--- android/app/src/main/java/pt/carvalho/apples/classifier/utilities/BitmapUtilities.kt
+++ android/app/src/main/java/pt/carvalho/apples/classifier/utilities/BitmapUtilities.kt
@@ -8,12 +8,16 @@ import kotlin.math.min
 fun Bitmap.centerSquareCrop(size: Int): Bitmap {
     val smallestSide = min(width, height)
 
-    if (smallestSide < size || size < 0) {
+    if (size < 0) {
         throw IllegalArgumentException("Expected size of bitmap is bigger than the actual bitmap.")
     }
 
-    val x = (width - size) / 2
-    val y = (height - size) / 2
+    return if (smallestSide < size) {
+        Bitmap.createScaledBitmap(this, size, size, false)
+    } else {
+        val x = (width - size) / 2
+        val y = (height - size) / 2
 
-    return Bitmap.createBitmap(this, x, y, size, size)
+        Bitmap.createBitmap(this, x, y, size, size)
+    }
 }
